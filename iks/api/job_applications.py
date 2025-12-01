e1779b9b247a1e9e69d29f6e06288d1cf77513df
Ahmed AbdulRahman <ahmed.abdulrahman2@vodafone.com>
2025-12-01 22:07:58 +0200

    fix: form and imporve error

--- iks/api/job_applications.py
+++ iks/api/job_applications.py
@@ -1,22 +1,25 @@
 import frappe
 
 
-@frappe.whitelist(allow_guest=True, methods=["POST"])
+@frappe.whitelist(allow_guest=True, xss_safe=True, methods=["POST"])
 def apply_for_job():
-	request_file = frappe.request.files["cv"]
-	content = request_file.read()
-	file = frappe.get_doc(
-		{
-			"doctype": "File",
-			"file_name": request_file.filename,
-			"is_private": 1,
-			"content": content,
-		}
-	)
-	file.save(ignore_permissions=True)
+	try:
+		request_file = frappe.request.files["cv"]
+		content = request_file.read()
+		file = frappe.get_doc(
+			{
+				"doctype": "File",
+				"file_name": request_file.filename,
+				"is_private": 1,
+				"content": content,
+			}
+		)
+		file.save(ignore_permissions=True)
+		job_application = frappe.new_doc("IKS Job Application")
+		job_application.title = "new job"
+		job_application.cv = file.file_url
+		job_application.save(ignore_permissions=True)
+		return {"success": True}
 
-	job_application = frappe.new_doc("Job Application")
-	job_application.title = "new job"
-	job_application.cv = file.file_url
-	job_application.save(ignore_permissions=True)
-	return {"success": True}
+	except:
+		return {"success": False}
