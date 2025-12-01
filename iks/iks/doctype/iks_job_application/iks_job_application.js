62cb28aa0c2a7e7f0f4db3c2a963c617d8025b11
Ahmed AbdulRahman <ahmed.abdulrahman2@vodafone.com>
2025-12-02 00:49:05 +0200

    feat: added doctype

---
+++ iks/iks/doctype/iks_job_application/iks_job_application.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ahmed AbdulRahman and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("IKS Job Application", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ iks/iks/doctype/iks_job_application/test_iks_job_application.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Ahmed AbdulRahman and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestIKSJobApplication(FrappeTestCase):
+	pass
